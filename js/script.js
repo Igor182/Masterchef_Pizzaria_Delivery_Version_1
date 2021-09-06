957987cdd87a3c19c9ad6b9fc50af57c351ec8dd
Igor Mello <igormello_2014@hotmail.com>
2021-09-05 23:02:04 -0300

    atualizacao site masterchef

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
     $(".hamburguer").click(function () {
-        $(this).toggleClass('active');
-        $(".mennu").toggleClass('active');
+        $(this).toggleClass("active");
+        $(".mennu").toggleClass("active");
    });
 });
+
+
+$('.bloco').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+      ]
+  });
