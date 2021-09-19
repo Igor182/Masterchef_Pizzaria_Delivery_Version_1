6b35ea2b8758984472861451d4b10991a60d6c2d
Igor Mello <igormello_2014@hotmail.com>
2021-09-19 20:11:31 -0300

    atualização (remoção temporária do carrosel e ajuste nas tags IMG)

--- js/script.js
+++ js/script.js
@@ -5,27 +5,3 @@ $(document).ready(function () {
    });
 });
 
-
-$('.bloco').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-          }
-        }
-      ]
-  });
